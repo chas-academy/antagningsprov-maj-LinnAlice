b7b0df09ca40b51d4e0786552be40cae4fdfc396
LinnAlice <linnolsson16@hotmail.com>
2025-05-10 13:37:26 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -1,12 +1,15 @@
-
 function uppg4() {
   
     // deklarera ett objekt med namnet person
     // objektet ska ha två properties, name och age
     // tilldela age 25
     // skriv ut med dotnotation åldern i konsolen
-
-
+    const person = {
+        name: "Linn",
+        age: 25,
+    };
+    console.log(person.age);
 }
+uppg4();
 
 module.exports = { uppg4 };
