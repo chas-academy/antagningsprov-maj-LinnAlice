973cda0716c3d87874f80715302ab698924beb8b
LinnAlice <linnolsson16@hotmail.com>
2025-05-10 13:37:59 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,14 @@
-
-
 function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
- 
+    const names = ["Linn", "Alfred", "Calle", "Niklas", "Henrik"];
+
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 }
+uppg6();
 
 module.exports = { uppg6 };
