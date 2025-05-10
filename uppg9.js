1cdf24567a85c407e5cdcdb3d299d31409c81ed7
LinnAlice <linnolsson16@hotmail.com>
2025-05-10 14:18:08 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -7,23 +7,18 @@ function uppg9 (){
     // anropa funktionen och skicka med en array som argument
 
     function sort(arr) {
-        const resultat = [];
 
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] % 2 === 0) {
-                console.log(arr[i] + "jämt");
-                resultat.push([arr[i], "jämt"])
+                console.log(arr[i]);
             } else {
-                console.log(arr[i] + "udda");
-                resultat.push([arr[i], "udda"])
-
+                console.log(arr[i]);
             }
         }
-        return resultat
     };
 
     const siffror = [5,20,13,72,3,1,92,66,73];
-    return sort(siffror);
+    sort(siffror);
   
 }
 uppg9();
