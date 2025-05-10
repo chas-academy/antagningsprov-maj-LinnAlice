67fd34239351006d97e327ebf2661eceb4126a96
LinnAlice <linnolsson16@hotmail.com>
2025-05-10 14:02:47 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -10,9 +10,11 @@ function uppg9 (){
 
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] % 2 === 0) {
-                console.log(arr[i] + "\njämt");
+                console.log(arr[i]);
+                console.log("jämt");
             } else {
-                console.log(arr[i] + "\nudda");
+                console.log(arr[i]);
+                console.log("udda");
             }
         }
     };
