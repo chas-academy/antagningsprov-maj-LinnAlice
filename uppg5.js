44f239f739e8f87cdfac62678be488f1058ae4dc
LinnAlice <linnolsson16@hotmail.com>
2025-05-10 13:37:44 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,16 @@
-
 function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+    const a = 5;
+    const b = 10;
 
-
-
+    if (a > b) {
+        console.log("Det första talet är större än det andra");
+    } else {
+        console.log("Det andra talet är större än det första");
+    }
 }
+uppg5();
 
 module.exports = { uppg5 };
