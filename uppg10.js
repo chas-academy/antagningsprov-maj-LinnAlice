5a0334a3d9569c9e698e3af7eca07b836b3c9a59
LinnAlice <linnolsson16@hotmail.com>
2025-05-10 13:39:02 +0200

    Update uppg10.js

--- uppg10.js
+++ uppg10.js
@@ -7,11 +7,34 @@ function uppg10(){
     // smallOdd ska innehålla tal mindre än 10 och udda
     // other ska innehålla alla andra tal
     // returnera ett objekt med de tre arrayerna
-
-  
     
+    const bigEven = [];
+    const smallOdd = [];
+    const other = [];
+
+
+    for (let i = 0; i < numbers.length; i++) {
+      // Jämnför stora och jämna tal
+        if(numbers[i] > 10 && numbers[i] % 2 == 0) {
+          bigEven.push(numbers[i])
+        }
+        // Jämnför små och ojämna tal
+        else if (numbers[i] < 10 && numbers[i] % 2 == 1){
+          smallOdd.push(numbers[i])
+        }
+        // Resterande tal som inte uppfyller ovanstående krav
+        else {
+          other.push(numbers[i])
+        }
+    }
+
+    const arrays = {bigEven, smallOdd, other}
+
+    return arrays;
   
   }
+
+  console.log(uppg10());
   
   module.exports = { uppg10 };
   
